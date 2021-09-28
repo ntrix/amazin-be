c5098f857499d98132515e9589118115d9b2fd15
ntrix <ntrix@NTien.com>
2021-09-28 13:18:23 +0200

    privacy: admin db api

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,16 +3,22 @@ import asyncHandler from "express-async-handler";
 import productControllers from "../controllers/productControllers.js";
 import { checkToken } from "../auth/token.js";
 import { isAdmin, isSellerOrAdmin } from "../auth/rolls.js";
+import { config } from "dotenv";
 
+config();
 const productRoute = express.Router();
 
 productRoute.get(
-  "/admin-backup-my-db" + process.env.DB_key,
+  `/admin-backup-my-db/${process.env.DB_key}`,
+  checkToken,
+  isAdmin,
   asyncHandler(productControllers.adminBackup)
 );
 
 productRoute.get(
-  "/admin-change-my-db" + process.env.DB_key,
+  `/admin-change-my-db/${process.env.DB_key}`,
+  checkToken,
+  isAdmin,
   asyncHandler(productControllers.adminUpdate)
 );
 
