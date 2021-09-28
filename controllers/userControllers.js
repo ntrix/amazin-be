cad5ba05b39b9c607a41a7c725844d042e5d89ae
ntrix <ntrix@NTien.com>
2021-09-28 15:48:24 +0200

    fix verify new seller account

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -166,10 +166,10 @@ const userControllers = {
       if (user.isSeller || req.body.verify) {
         user.isSeller = true; //verify and apply new seller profile from user acc
         user.seller.name =
-          req.body.seller.name || user.seller.name || user.name;
-        user.seller.logo = req.body.seller.logo || user.seller.logo;
+          req.body.seller?.name || user.seller.name || user.name;
+        user.seller.logo = req.body.seller?.logo || user.seller.logo;
         user.seller.description =
-          req.body.seller.description || user.seller.description;
+          req.body.seller?.description || user.seller.description;
       }
 
       if (req.body.oldPassword) {
